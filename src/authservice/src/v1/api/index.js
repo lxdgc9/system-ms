fe71132d387e551f394b4b096a7082016cf28a42
DANH PHI LONG <longsf9@gmail.com>
2023-02-10 18:20:49 +0700

    delete auth service

--- src/authservice/src/v1/api/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const validateLogin = require("../validations/login");
-const login = require("../handlers/login");
-
-const router = express.Router();
-
-router.post("/login", validateLogin(), login);
-
-module.exports = router;
--- src/authservice/src/v1/models/account.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const accountSchema = require("../schemas/account");
-
-const AccountModel = mongoose.model("account", accountSchema);
-
-module.exports = AccountModel;
